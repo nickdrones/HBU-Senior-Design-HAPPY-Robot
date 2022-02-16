c17d51a13aa2d51ba05d4d1c7347d688996556c7
Nick Belbas <nickbelbas@earthlink.net>
2022-02-16 12:19:47 -0600

    Added serial comm initialzation

--- Navi.py
+++ Navi.py
@@ -1,7 +1,15 @@
 # GPS CODE SERIAL READ
+import time
+import serial
 def setup():
-    print("Initializing GPS...")
+    print("Initializing Serial Comms to GPS...")
+
+    global serial_port 
+    serial_port = serial.Serial(port="/dev/ttyTHS1",baudrate=115200,bytesize=serial.EIGHTBITS,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,)
+    
     print("Initializing GPS Complete")
 
 if __name__ == '__main__':
 	setup()
+
+#Help: https://github.com/JetsonHacksNano/UARTDemo/blob/master/uart_example.py
