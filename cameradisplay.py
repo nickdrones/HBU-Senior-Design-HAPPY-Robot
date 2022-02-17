97bb026aca069f7ea6ed85f936249f55569a4bfa
Nick Belbas <nickbelbas@earthlink.net>
2022-02-17 13:39:18 -0600

    Update cameradisplay.py

--- cameradisplay.py
+++ cameradisplay.py
@@ -10,6 +10,15 @@ if __name__ == '__main__':
         try:
             # read the camera image
             frame = camera.read()
+            font = cv2.FONT_HERSHEY_SIMPLEX
+  
+            cv2.putText(frame, 
+                'TEXT ON VIDEO', 
+                (50, 50), 
+                font, 1, 
+                (0, 255, 255), 
+                2, 
+                cv2.LINE_4)
             # display the frame
             cv2.imshow("Video Frame", frame)
             if cv2.waitKey(25) & 0xFF == ord('q'):
