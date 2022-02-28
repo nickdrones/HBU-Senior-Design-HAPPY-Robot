05b756a07fadec3f6d9ddba5cf2ba525c318ca40
Nick Belbas <nickbelbas@earthlink.net>
2022-02-28 10:25:47 -0600

    Update serialtest.py

--- serialtest.py
+++ serialtest.py
@@ -21,7 +21,7 @@ try:
     #serial_port.write("UART Demonstration Program\r\n".encode())
     #serial_port.write("NVIDIA Jetson Nano Developer Kit\r\n".encode())
     databit = ""
-    serial_port.write("a")
+    serial_port.write("a".encode())
     while True:
         if serial_port.inWaiting() > 0:
             data = serial_port.read()
