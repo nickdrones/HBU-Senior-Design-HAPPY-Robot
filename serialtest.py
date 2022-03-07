b1dac4ad71a8e4fecb3977ed33252f29f893d8e2
Eragon115 <leipherikson@gmail.com>
2022-03-07 11:54:29 -0600

    Update serialtest.py

--- serialtest.py
+++ serialtest.py
@@ -2,8 +2,6 @@
 import time
 import serial
 
-print("UART Demonstration Program")
-print("NVIDIA Jetson Nano Developer Kit")
 
 serial_port = serial.Serial(
     port="/dev/ttyTHS1",
@@ -15,39 +13,42 @@ serial_port = serial.Serial(
 # Wait a second to let the port initialize
 time.sleep(2)
 
-try:
-    # Send a simple header
-    #serial_port.write("UART Demonstration Program\r\n".encode())
-    #serial_port.write("NVIDIA Jetson Nano Developer Kit\r\n".encode())
-    databits = []
-    while True:
-        print(serial_port.readline())
+
+while True:
+    try:
+        # Send a simple header
+        print("Start of Program")
+        
+        ser_bytes = serial_port.readline()
+        decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
+        print(decoded_bytes)
+
         # bytesToRead = serial_port.inWaiting()
         # data = serial_port.read(bytesToRead)
         # databits.append(data)
-        #print(data)
-            #databit += data
-            # if we get a carriage return, add a line feed too
-            # \r is a carriage return; \n is a line feed
-            # This is to help the tty program on the other end 
-            # Windows is \r\n for carriage return, line feed
-            # Macintosh and Linux use \n
-        #if data == "\r".encode():
+        # print(data)
+        # databit += data
+        # if we get a carriage return, add a line feed too
+        # \r is a carriage return; \n is a line feed
+        # This is to help the tty program on the other end 
+        # Windows is \r\n for carriage return, line feed
+        # Macintosh and Linux use \n
+        # if data == "\r".encode():
         # if len(databits) > 100:
-            # # For Windows boxen on the other end
-            # #serial_port.write("\n".encode())
-            # print("Complete message: ")
-            # print(databits)
-            # break
+        # #For Windows boxen on the other end
+        # #serial_port.write("\n".encode())
+        # print("Complete message: ")
+        # print(databits)
+        # break
 
 
-except KeyboardInterrupt:
-    print("Exiting Program")
+    except KeyboardInterrupt:
+        print("Exiting Program")
 
-except Exception as exception_error:
-    print("Error occurred. Exiting Program")
-    print("Error: " + str(exception_error))
+    except Exception as exception_error:
+        print("Error occurred. Exiting Program")
+        print("Error: " + str(exception_error))
 
-finally:
-    serial_port.close()
-    pass
+    finally:
+        serial_port.close()
+        pass
