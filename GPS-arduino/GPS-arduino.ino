61fb789cab85d33fcb89ab92f829ad99d5612a4b
Nick Belbas <nickbelbas@earthlink.net>
2022-04-06 12:40:14 -0500

    gps serial work

--- GPS-arduino/GPS-arduino.ino
+++ GPS-arduino/GPS-arduino.ino
@@ -38,32 +38,32 @@ void loop()
   //The module only responds when a new position is available
   incomingByte = Serial.read();
   // send data only when you receive data:
-  if (incomingByte != 10) {
+  //if (incomingByte != 10) {
 
-    //This is for 'O' longitude
-    if (incomingByte == 79) {
-      // say what you got:
-      //Serial.print("The longitude is: ");
-      Serial.println(longitude);
-    }
+  //This is for 'O' longitude
+  //if (incomingByte == 79) {
+  // say what you got:
+  //Serial.print("The longitude is: ");
+  //}
 
-    //This is for 'A' latitude
-    if (incomingByte == 65) {
-      // say what you got:
-      //Serial.print("The latitude is: ");
-      Serial.println(latitude);
-    }
-  }
+  //This is for 'A' latitude
+  //if (incomingByte == 65) {
+  // say what you got:
+  //Serial.print("The latitude is: ");
+
+
+  //}
+  //}
   if (millis() - lastTime > 1000)
   {
     lastTime = millis(); //Update the timer
 
-    //latitude = myGNSS.getLatitude();
+    latitude = myGNSS.getLatitude();
     //Serial.print(F("Lat: "));
     //Serial.print(latitude);
     //Serial.print(",");
 
-    //longitude = myGNSS.getLongitude();
+    longitude = myGNSS.getLongitude();
     //Serial.print(F(" Long: "));
     //Serial.println(longitude);
     //Serial.print(F(" (degrees * 10^-7)"));
@@ -79,8 +79,9 @@ void loop()
 
     //Serial.println("Please input an 'O' (for longitude) or 'A' (for latitude)");
     //read the incoming byte:
-
-    if(Serial.available() > 0)
+    Serial.println(longitude);
+    Serial.println(latitude);
+    if (Serial.available() > 0)
     {
       //char messageBuffer[32];
       //int size = Serial.readBytesUntil('\n', messageBuffer, 32);
---
+++ serialtest2.py
@@ -0,0 +1,26 @@
+import serial
+import time
+
+print("UART Demonstration Program")
+print("NVIDIA Jetson Nano Developer Kit")
+
+
+serial_port = serial.Serial(
+    port="/dev/ttyTHS1",
+    baudrate=115200,
+    bytesize=serial.EIGHTBITS,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+)
+# Wait a second to let the port initialize
+time.sleep(1)
+
+try:
+    # Send a message to the Arduino
+    serial_port.write("500 500".encode())
+    while True:
+        if serial_port.inWaiting() > 0:
+            data = serial_port.readline().decode()
+            print(data)
+except:
+    print("lmao error git rekt")
