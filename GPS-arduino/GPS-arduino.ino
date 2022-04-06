c3729bb87c33aa106d0e1c232195f1017dcc7638
Nick Belbas <nickbelbas@earthlink.net>
2022-04-06 13:13:48 -0500

    added feature to print long and lat at same time

--- GPS-arduino/GPS-arduino.ino
+++ GPS-arduino/GPS-arduino.ino
@@ -39,6 +39,7 @@ void loop()
     String commandFromJetson = Serial.readStringUntil(TERMINATOR);
     if (commandFromJetson.indexOf("O") >= 0) {Serial.println(longitude);}
     if (commandFromJetson.indexOf("A") >= 0) {Serial.println(latitude);}
+    if (commandFromJetson.indexOf("B") >= 0) {Serial.println(String(longitude) + "," + String(latitude));}
   }
   if (millis() - lastTime > 1000)
   {
