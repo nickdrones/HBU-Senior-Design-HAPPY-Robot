967a094a1eab65d9c76b2d88675da269bb80e2f5
Nick Belbas <nickbelbas@earthlink.net>
2022-04-06 12:47:31 -0500

    different serial port

--- serialtest2.py
+++ serialtest2.py
@@ -6,7 +6,7 @@ print("NVIDIA Jetson Nano Developer Kit")
 
 
 serial_port = serial.Serial(
-    port="/dev/ttyTHS2",
+    port="/dev/ttyACM0",
     baudrate=115200,
     bytesize=serial.EIGHTBITS,
     parity=serial.PARITY_NONE,
