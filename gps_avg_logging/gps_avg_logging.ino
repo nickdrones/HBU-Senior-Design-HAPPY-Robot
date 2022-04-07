a17793e61dcf15e5e046ed18cea95449e1c26142
Nick Belbas <nickbelbas@earthlink.net>
2022-04-07 09:42:12 -0500

    Create gps_avg_logging.ino
    Created code that will return a rolling average of the gps coordinates to help avoid gps drift while measuring positions for destinations and waypoints

---
+++ gps_avg_logging/gps_avg_logging.ino
@@ -0,0 +1,51 @@
+#include <SparkFun_u-blox_GNSS_Arduino_Library.h> //http://librarymanager/All#SparkFun_u-blox_GNSS
+#include <u-blox_config_keys.h>
+#include <Wire.h> //Needed for I2C to GNSS
+
+SFE_UBLOX_GNSS myGNSS;
+
+long lastTime = 0; //Simple local timer. Limits amount if I2C traffic to u-blox module.
+
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial); //Wait for user to open terminal
+
+  Wire.begin();
+
+  if (myGNSS.begin() == false) //Connect to the u-blox module using Wire port
+  {
+    Serial.println(F("u-blox GNSS not detected at default I2C address. Please check wiring. Freezing."));
+    while (1);
+  }
+
+  myGNSS.setI2COutput(COM_TYPE_UBX); //Set the I2C port to output UBX only (turn off NMEA noise)
+  //myGNSS.saveConfiguration(); //Optional: Save the current settings to flash and BBR
+}
+
+long last_long = 0;
+long last_lat = 0;
+long latitude = 0;
+long longitude = 0;
+long tot_latitude = 0;
+long tot_longitude = 0;
+String sdata = "";
+int numOfLoops = 1;
+int incomingByte = 0; // for incoming serial data
+
+void loop()
+{
+  if (millis() - lastTime > 1000)
+  {
+    lastTime = millis(); //Update the timer
+    latitude = myGNSS.getLatitude();
+    longitude = myGNSS.getLongitude();
+    tot_latitude += latitude;
+    tot_longitude += longitude;
+    Serial.println("Number of loops: " + String(numOfLoops));
+    Serial.println("Current: " + String(longitude) + " , " + String(latitude));
+    Serial.println("Rolling Average: " + String(tot_longitude/numOfLoops) + " , " + String(tot_latitude/numOfLoops));
+    Serial.println("***************************************************************************");
+    numOfLoops++;
+  }
+}
