ce231a3f138ef67cf60a426ca4bb4f4be5b6d107
Nick Belbas <nickbelbas@earthlink.net>
2022-04-11 22:37:51 -0500

    knight rider test

--- adc_test.py
+++ adc_test.py
@@ -4,12 +4,23 @@ import time
 
 i2c = board.I2C()
 bc24 = Bicolor24(i2c)
+display.brightness = 1
 
-bc24[0] = bc24.LED_RED
-bc24[1] = bc24.LED_GREEN
-bc24[2] = bc24.LED_YELLOW
-bc24[3] = bc24.LED_OFF
+while 1==1:
+    for x in range(24):
+        bc24[x] = bc24[0] = bc24.LED_RED
+        time.sleep(0.05)
+        bc24[x] = bc24.LED_OFF
+    for x in range(24):
+        bc24[24-x] = bc24[0] = bc24.LED_RED
+        time.sleep(0.05)
+        bc24[24-x] = bc24.LED_OFF
 
-time.sleep(1)
+#bc24[0] = bc24.LED_RED
+#bc24[1] = bc24.LED_GREEN
+#bc24[2] = bc24.LED_YELLOW
+#bc24[3] = bc24.LED_OFF
 
-bc24.fill(bc24.LED_GREEN)
+#time.sleep(1)
+
+#bc24.fill(bc24.LED_GREEN)
