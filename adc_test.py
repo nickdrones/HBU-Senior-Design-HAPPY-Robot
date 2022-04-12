6a60dd7af4e5b65343e40682511442ceacfbcbe0
Nick Belbas <nickbelbas@earthlink.net>
2022-04-11 22:25:26 -0500

    Update adc_test.py

--- adc_test.py
+++ adc_test.py
@@ -6,6 +6,7 @@ import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 ads = ADS.ADS1115(i2c)
 chan = AnalogIn(ads, ADS.P0)
+ads.mode = Mode.SINGLE
 while 1==1:
     print(chan.value, chan.voltage)
     time.sleep(1)
