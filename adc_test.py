a58c32059131d35111114492bed76f1614ea64bd
Nick Belbas <nickbelbas@earthlink.net>
2022-04-11 22:42:35 -0500

    Update adc_test.py

--- adc_test.py
+++ adc_test.py
@@ -16,12 +16,12 @@ bc24 = Bicolor24(i2c)
 while 1==1:
     for x in range(23):
         bc24[x]  = bc24.LED_RED
-        time.sleep(0.05)
+        time.sleep(0.03)
         bc24[x] = bc24.LED_OFF
     for x in range(23):
-        bc24[24-x]  = bc24.LED_RED
-        time.sleep(0.05)
-        bc24[24-x] = bc24.LED_OFF
+        bc24[23-x]  = bc24.LED_RED
+        time.sleep(0.03)
+        bc24[23-x] = bc24.LED_OFF
 
 #bc24[0] = bc24.LED_RED
 #bc24[1] = bc24.LED_GREEN
