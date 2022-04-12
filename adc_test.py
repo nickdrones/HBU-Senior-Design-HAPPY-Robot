d593f4e10b1c756c0674f1bd5254e1ce772577fb
Nick Belbas <nickbelbas@earthlink.net>
2022-04-11 22:41:21 -0500

    Update adc_test.py

--- adc_test.py
+++ adc_test.py
@@ -1,17 +1,25 @@
 import board
 from adafruit_ht16k33.bargraph import Bicolor24
 import time
+import signal
+
+def keyboardInterruptHandler(signal, frame):
+    print("KeyboardInterrupt (ID: {}) has been caught. Cleaning up...".format(signal))
+    #ALL CODE TO RUN WHILE STOPPING ROBOT GOES HERE
+    bc24.fill(bc24.LED_OFF)
+    exit(0)
+signal.signal(signal.SIGINT, keyboardInterruptHandler)
 
 i2c = board.I2C()
 bc24 = Bicolor24(i2c)
 
 while 1==1:
-    for x in range(22):
-        bc24[x] = bc24[0] = bc24.LED_RED
+    for x in range(23):
+        bc24[x]  = bc24.LED_RED
         time.sleep(0.05)
         bc24[x] = bc24.LED_OFF
-    for x in range(22):
-        bc24[24-x] = bc24[0] = bc24.LED_RED
+    for x in range(23):
+        bc24[24-x]  = bc24.LED_RED
         time.sleep(0.05)
         bc24[24-x] = bc24.LED_OFF
 
