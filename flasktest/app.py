b565c4c42ffadb949b487a31cc0e031236089798
Nick Belbas <nickbelbas@earthlink.net>
2022-04-12 22:56:41 -0500

    Update app.py

--- flasktest/app.py
+++ flasktest/app.py
@@ -38,5 +38,5 @@ def inject_load():
 def update_load():
     with app.app_context():
         while True:
-            time.sleep(0.5)
+            time.sleep(1)
             turbo.push(turbo.replace(render_template('loadavg.html'), 'load'))
