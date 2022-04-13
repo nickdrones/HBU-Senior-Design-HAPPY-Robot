c90e82f39d153f8c3db64bfa5ea66b83df79db6c
Nick Belbas <nickbelbas@earthlink.net>
2022-04-12 22:53:45 -0500

    added auto update

--- flasktest/app.py
+++ flasktest/app.py
@@ -2,8 +2,17 @@ import random
 import re
 import sys
 from flask import Flask, render_template
+from turbo_flask import Turbo
+import threading
+import time
 
 app = Flask(__name__)
+turbo = Turbo(app)
+
+
+@app.before_first_request
+def before_first_request():
+    threading.Thread(target=update_load).start()
 
 @app.route('/')
 def index():
@@ -21,3 +30,9 @@ def inject_load():
     else:
         load = [int(random.random() * 100) / 100 for _ in range(3)]
     return {'load1': load[0], 'load5': load[1], 'load15': load[2]}
+
+def update_load():
+    with app.app_context():
+        while True:
+            time.sleep(5)
+            turbo.push(turbo.replace(render_template('loadavg.html'), 'load'))
