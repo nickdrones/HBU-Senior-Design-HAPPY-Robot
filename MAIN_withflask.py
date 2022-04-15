a0b8ecfd2c04c140fd71d3b41302c07e9044bcf3
Nick Belbas <nickbelbas@earthlink.net>
2022-04-14 22:50:44 -0500

    Update MAIN_withflask.py

--- MAIN_withflask.py
+++ MAIN_withflask.py
@@ -257,8 +257,9 @@ def update_load():
             turbo.push(turbo.replace(render_template('onthejob.html'), 'onthejob'))
             turbo.push(turbo.replace(render_template('nearestdestination.html'), 'nearestdestination'))
 
-app.run(debug=True, port=8080, host="172.17.21.145")
+if __name__ == "__main__":
+    app.run(debug=True, port=8080, host="172.17.21.145")
 
 
 
-quit()
+#quit()
