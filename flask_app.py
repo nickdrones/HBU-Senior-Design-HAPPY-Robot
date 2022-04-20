04f40e9c85371fcbca4edcf0bba9c454cc4c362b
Nick Belbas <nickbelbas@earthlink.net>
2022-04-19 21:09:06 -0500

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -130,5 +130,5 @@ def video_feed():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
-    #app.run(debug=True, port=8080, host="172.17.21.145")
+    #app.run(debug=True, port=8080)
+    app.run(debug=True, port=8080, host="172.17.21.145")
