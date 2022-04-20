b9f74e2bf92df7cd5e9faae74dce83c293919d1c
Nick Belbas <nickbelbas@earthlink.net>
2022-04-20 11:53:55 -0500

    Update webpage_display_camera.py

--- webpage_display_camera.py
+++ webpage_display_camera.py
@@ -70,4 +70,4 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=2204, threaded=True)
+    app.run(host='0.0.0.0', port=2204, threaded=True, debug=False)
